f5317748261a1a1b4f4194e7a48710e48fb50293
Tanakaryuki <46374705+Tanakaryuki@users.noreply.github.com>
2021-09-16 23:12:27 +0900

    html,cssのお引っ越し

--- OnMuse/MySite/post/urls.py
+++ OnMuse/MySite/post/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'post'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('_create/', views.post_create, name='post_create'),
+    path('list', views.post_list, name='post_list'),
+    path('create', views.post_create, name='post_create'),
 ]
