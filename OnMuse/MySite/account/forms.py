084ab3c21135312d1688d3f0b5efe079dac5dfab
Tanakaryuki <46374705+Tanakaryuki@users.noreply.github.com>
2021-09-29 21:41:39 +0900

    signupが出来るように

---
+++ OnMuse/MySite/account/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import CustomUser
+
+
+class SignUpForm(UserCreationForm):
+    last_name = forms.CharField(
+        max_length=30,
+        required=False,
+        help_text='オプション',
+        label='苗字',
+    )
+    first_name = forms.CharField(
+        max_length=30,
+        required=False,
+        help_text='オプション',
+        label='名前',
+    )
+    email = forms.EmailField(
+        max_length=254,
+        help_text='必須 有効なメールアドレスを入力してください。',
+        label='Eメールアドレス',
+    )
+    introduction = forms.CharField(
+        max_length=200,
+        required=False,
+        label='自己紹介',
+    )
+    link = forms.URLField(
+        max_length=200,
+        required=False,
+        help_text='有効なリンクを入力してください',
+        label='Eメールアドレス',
+    )
+    icon = forms.ImageField(
+        required=False,
+        label='アイコン'
+    )
+    birthday = forms.DateTimeField(
+        required=False,
+        label='生年月日'
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'password1', 'password2', 'last_name', 'first_name',  'email','introduction','link','icon','birthday' )
--- OnMuse/MySite/account/views.py
+++ OnMuse/MySite/account/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm
 
 """ def login(request):
     return render(request,'account/login.html',)
@@ -8,7 +10,16 @@ def logout(request):
     return render(request,'../home',) """
 
 def signup(request):
-    return render(request,'account/signup.html',)
+    if request.method == 'POST':
+        form = SignUpForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            return redirect('home:choice')
+    else:
+        form = SignUpForm()
+    return render(request, 'account/signup_test.html', {'form': form})
 
 @login_required
 def profile(request):
