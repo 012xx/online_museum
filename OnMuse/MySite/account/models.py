dbcdad398c4e7dc569315c1ca7beb2910bbf9e34
Tanakaryuki <46374705+Tanakaryuki@users.noreply.github.com>
2021-11-01 17:12:17 +0900

    誕生日の単位を秒から日に変更

--- OnMuse/MySite/account/models.py
+++ OnMuse/MySite/account/models.py
@@ -12,4 +12,4 @@ class CustomUser(AbstractUser):
     introduction = models.TextField(verbose_name='自己紹介',max_length=200,blank=True,null=True,default="よろしくお願いします")
     link = models.URLField(verbose_name='リンク',max_length=200,blank=True,null=True)
     icon = models.ImageField(verbose_name='アイコン画像',upload_to=icon_path,blank=True,null=False,default = '../static/picture/8074e2c65a3ab65d0ce7b482795b7ac0.jpg')
-    birthday = models.DateTimeField(verbose_name='生年月日',blank=True,null=True)
+    birthday = models.DateField(verbose_name='生年月日',blank=True,null=True)
