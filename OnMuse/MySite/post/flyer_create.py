7fd162c1f8b0cf6bcd40e71ad3d509ed5eca2fcc
Tanakaryuki <46374705+Tanakaryuki@users.noreply.github.com>
2021-11-03 23:21:29 +0900

    フライヤー5の追加

--- OnMuse/MySite/post/flyer_create.py
+++ OnMuse/MySite/post/flyer_create.py
@@ -10,6 +10,8 @@ def flyer(num,image,title,user):
         return flyer3(image,title,user)
     elif num == 4:
         return flyer4(image,title,user)
+    elif num == 5:
+        return flyer5(image,title,user)
 
 def len_count(text):
     count = 0
@@ -207,6 +209,53 @@ def flyer4(image,title,user):
         draw.text((12,580),title.center(19 - ZEN_count(title),' '),fill='black',font = font)
     font = ImageFont.truetype("msgothic.ttc",30)
     draw.text((0,650),user.rjust(32,' '),fill='black',font = font)
+
+    name = "medias/flyers/{}.png".format(str(uuid.uuid4()))
+    origin.save(name,quality = 95)#保存先のパス
+
+    return name[7:]
+
+    #staticから705 * 500の画像を複製
+    #開いた画像の短辺を基準にリサイズ(おそらくはみ出す)
+    #はみ出した分、作品が真ん中に来るように切り取り
+    #static画像に張り付け
+    #タイトル、ユーザーを描写
+
+def flyer5(image,title,user):
+    #ユーザ名10文字、タイトル10文字
+    image = Image.open(image)#ユーザの画像
+    #origin = Image.open('origin.png')#static画像
+    origin = Image.new('RGBA',(500,705),(230,62,57)) #で同じことが可能
+
+    image_copy = image.copy()
+
+    WIDTH = 500
+    HEIGHT = 675
+
+    width,height = image_copy.size#リサイズ＆切り取り
+    if(HEIGHT/height > WIDTH/width):#縦を基準に
+        mag = HEIGHT / height
+        image_copy = image_copy.resize((int(mag * width),int(mag * height)),Image.LANCZOS)
+        dif = int((mag * width - WIDTH) / 2)#はみ出し測定
+        image_copy = image_copy.crop((dif,0,WIDTH + dif,HEIGHT))
+    else:
+        mag = WIDTH / width
+        image_copy = image_copy.resize((int(mag * width),int(mag * height)),Image.LANCZOS)
+        dif = int((mag * height - HEIGHT) / 2)#はみ出し測定
+        image_copy = image_copy.crop((0,dif,WIDTH,HEIGHT + dif))
+
+    origin.paste(image_copy,(0,0))#貼り付け
+
+    draw = ImageDraw.Draw(origin)
+    #もしタイトルが奇数だったら9埋めにしてずらす
+    #偶数の場合今まで通り10埋め
+    font = ImageFont.truetype("msgothic.ttc",50)
+    if len_count(title) % 2 == 0:
+        draw.text((0,300),title.center(20 - ZEN_count(title),' '),fill='black',font = font)
+    else:
+        draw.text((12,300),title.center(19 - ZEN_count(title),' '),fill='black',font = font)
+    font = ImageFont.truetype("msgothic.ttc",30)
+    draw.text((0,400),user.rjust(32,' '),fill='black',font = font)
 
     name = "medias/flyers/{}.png".format(str(uuid.uuid4()))
     origin.save(name,quality = 95)#保存先のパス
