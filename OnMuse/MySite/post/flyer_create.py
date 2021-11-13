9de09fee89a26db43c66695e2ae20c7229e13006
Tanakaryuki <46374705+Tanakaryuki@users.noreply.github.com>
2021-11-13 23:18:46 +0900

    フライヤー1の更新

--- OnMuse/MySite/post/flyer_create.py
+++ OnMuse/MySite/post/flyer_create.py
@@ -1,5 +1,5 @@
 from PIL import Image,ImageDraw,ImageFont
-import uuid,unicodedata
+import uuid,unicodedata,datetime
 
 def flyer(num,image,title,user):
     if num == 1:
@@ -33,12 +33,12 @@ def flyer1(image,title,user):
     #ユーザ名10文字、タイトル10文字
     image = Image.open(image)#ユーザの画像
     #origin = Image.open('origin.png')#static画像
-    origin = Image.new('RGBA',(500,705),'white') #で同じことが可能
+    origin = Image.new('RGBA',(500,718),(225,225,225))
 
     image_copy = image.copy()
 
-    WIDTH = 500
-    HEIGHT = 428
+    WIDTH = 355
+    HEIGHT = 480
 
     width,height = image_copy.size#リサイズ＆切り取り
     if(HEIGHT/height > WIDTH/width):#縦を基準に
@@ -52,18 +52,25 @@ def flyer1(image,title,user):
         dif = int((mag * height - HEIGHT) / 2)#はみ出し測定
         image_copy = image_copy.crop((0,dif,WIDTH,HEIGHT + dif))
 
-    origin.paste(image_copy,(0,0))#貼り付け
+    origin.paste(image_copy,(145,0))#貼り付け
 
     draw = ImageDraw.Draw(origin)
+    draw.rectangle((30+2,450-2,310+2,690-2),fill=(225,225,225),outline=(255,255,255))
+    draw.line(((30,450),(310,450),(310,690)),fill=(225,225,225),width=3)
+    draw.line(((30,450),(30,690),(310,690)),fill=(225,225,225),width=3)
+    draw.line(((145,0),(145,480),(500,480)),fill=(0,0,255),width=1)
+    draw.line(((145,0),(500-2,0),(500-2,480)),fill=(0,0,255),width=1)
     #もしタイトルが奇数だったら9埋めにしてずらす
     #偶数の場合今まで通り10埋め
     font = ImageFont.truetype("msgothic.ttc",50)
-    if len_count(title) % 2 == 0:
-        draw.text((0,500),title.center(20 - ZEN_count(title),' '),fill='black',font = font)
-    else:
-        draw.text((12,500),title.center(19 - ZEN_count(title),' '),fill='black',font = font)
-    font = ImageFont.truetype("msgothic.ttc",30)
-    draw.text((0,650),user.rjust(32,' '),fill='black',font = font)
+    draw.text((50,520),title[:5],fill='black',font = font)
+    if len(title) > 5:
+        draw.text((50,570),title[5:10],fill='black',font = font)
+    font = ImageFont.truetype("msgothic.ttc",24)
+    time = datetime.datetime.now()
+    draw.text((0,600),(str(time)[:10] + "～" ).rjust(39,' '),fill='black',font = font)
+    draw.text((408,640),"OnMuse",fill='black',font = font)
+    draw.text((0,680),user.rjust(40,' '),fill='black',font = font)
 
     name = "medias/flyers/{}.png".format(str(uuid.uuid4()))
     origin.save(name,quality = 95)#保存先のパス
