264f28d4a99433fe36e08d2c4c4b3e55f38c597b
Tanakaryuki <46374705+Tanakaryuki@users.noreply.github.com>
2021-11-14 18:39:08 +0900

    フライヤー3の更新

--- OnMuse/MySite/post/flyer_create.py
+++ OnMuse/MySite/post/flyer_create.py
@@ -139,12 +139,15 @@ def flyer3(image,title,user):
     #ユーザ名10文字、タイトル10文字
     image = Image.open(image)#ユーザの画像
     #origin = Image.open('origin.png')#static画像
-    origin = Image.new('RGBA',(500,705),(255,204,0)) #で同じことが可能
+    origin = Image.new('RGBA',(500,718),(202,235,224))
+    circle = Image.new('RGBA',(500,718),(202,235,224))
+    draw = ImageDraw.Draw(circle)
+    draw.ellipse((30,86,30+440,86+440),fill = 255)
 
     image_copy = image.copy()
 
-    WIDTH = 500
-    HEIGHT = 385
+    WIDTH = 440
+    HEIGHT = 440
 
     width,height = image_copy.size#リサイズ＆切り取り
     if(HEIGHT/height > WIDTH/width):#縦を基準に
@@ -158,18 +161,22 @@ def flyer3(image,title,user):
         dif = int((mag * height - HEIGHT) / 2)#はみ出し測定
         image_copy = image_copy.crop((0,dif,WIDTH,HEIGHT + dif))
 
-    origin.paste(image_copy,(0,0))#貼り付け
+    origin.paste(image_copy,(30,86))#貼り付け
+    origin = Image.alpha_composite(origin,circle)
 
     draw = ImageDraw.Draw(origin)
     #もしタイトルが奇数だったら9埋めにしてずらす
     #偶数の場合今まで通り10埋め
     font = ImageFont.truetype("msgothic.ttc",50)
     if len_count(title) % 2 == 0:
-        draw.text((0,500),title.center(20 - ZEN_count(title),' '),fill='black',font = font)
+        draw.text((0,540),title.center(20 - ZEN_count(title),' '),fill=(38,38,38),font = font)
     else:
-        draw.text((12,500),title.center(19 - ZEN_count(title),' '),fill='black',font = font)
+        draw.text((12,540),title.center(19 - ZEN_count(title),' '),fill=(38,38,38),font = font)
     font = ImageFont.truetype("msgothic.ttc",30)
-    draw.text((0,650),user.rjust(32,' '),fill='black',font = font)
+    time = datetime.datetime.now()
+    draw.text((10,620),str(time)[:10] + "～" ,fill=(38,38,38),font = font)
+    draw.text((10,660),"OnMuse",fill=(38,38,38),font = font)
+    draw.text((340,640),user.rjust(10,' '),fill=(38,38,38),font = font)
 
     name = "medias/flyers/{}.png".format(str(uuid.uuid4()))
     origin.save(name,quality = 95)#保存先のパス
