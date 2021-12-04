a4a3ab88300335ffa4f589e4ab5ef5b4f8baecd7
Tanakaryuki <46374705+Tanakaryuki@users.noreply.github.com>
2021-12-04 22:41:05 +0900

    joinを作成

--- OnMuse/MySite/post/models.py
+++ OnMuse/MySite/post/models.py
@@ -20,11 +20,9 @@ class Post(models.Model):
     content = models.TextField(verbose_name='本文',max_length=140,blank=False,null=False)
     tag = models.ManyToManyField(Tag, verbose_name='タグ',blank=True)#Tagモデルと紐づけ
     relation = models.ManyToManyField('self', verbose_name='関連', blank=True)#Postモデルと紐づけ
-    #previews = 見られた回数(展示会を個々で見れるようになってから実装)
     created_at = models.DateTimeField(verbose_name='投稿時間',auto_now_add=True,null=False)
     id = models.UUIDField(verbose_name='作品ID',primary_key=True, default=uuid.uuid4, editable=False,null=False)
     flyer = models.ImageField(verbose_name='フライヤー',upload_to=flyers_path,blank=False,null=False,default = '../static/picture/8074e2c65a3ab65d0ce7b482795b7ac0.jpg')
-    #comments = いつか実装
     like = models.IntegerField(default=0) 
 
     '''
@@ -32,6 +30,17 @@ class Post(models.Model):
     nullはデータベース(入力しなくても進める)
     idは主キー
     '''
+    
+    def __str__(self):
+        return str(self.id)
+
+class Exhibition(models.Model):
+    author = models.CharField(verbose_name='作者',max_length=100,null=False,default="admin")
+    created_at = models.DateTimeField(verbose_name='投稿時間',auto_now_add=True,null=False)
+    id = models.UUIDField(verbose_name='作品ID',primary_key=True, default=uuid.uuid4, editable=False,null=False)
+    image = models.ImageField(verbose_name='画像',upload_to=images_path,blank=False,null=False,default = '../static/picture/8074e2c65a3ab65d0ce7b482795b7ac0.jpg')
+    like = models.IntegerField(default=0)
+    post_id = models.CharField(verbose_name='親ID',max_length=10,null=False,default="admin")
     
     def __str__(self):
         return str(self.id)
