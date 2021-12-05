bfb19b5fcbd3d139e398b372dc95ec732fa5c04b
012xx <night.fox.045@gmail.com>
2021-12-05 17:05:24 +0900

    仮

--- OnMuse/MySite/static/js/like.js
+++ OnMuse/MySite/static/js/like.js
@@ -1,17 +1,17 @@
-// クリックイベントの登録
-loveMe.addEventListener('click', (e) => {
+// // クリックイベントの登録
+// loveMe.addEventListener('click', (e) => {
 
-})
-// クリック時間の制御
-// いいね数をカウント
-let timesClicked = 0
+// })
+// // クリック時間の制御
+// // いいね数をカウント
+// let timesClicked = 0
 
-// クリックイベントの登録
+// // クリックイベントの登録
  
-// ハートの作成
-const createHeart = (e) => {
-...
+// // ハートの作成
+// const createHeart = (e) => {
+// ...
  
-  // いいね数を増加して挿入
-  times.innerHTML = ++timesClicked
-}
+//   // いいね数を増加して挿入
+//   times.innerHTML = ++timesClicked
+// }
