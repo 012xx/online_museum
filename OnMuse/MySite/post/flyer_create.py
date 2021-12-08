3acb0a460e5424a4726eeebade4bb97ea21bffeb
Tanakaryuki <46374705+Tanakaryuki@users.noreply.github.com>
2021-12-09 01:14:03 +0900

    フライヤー4の改良

--- OnMuse/MySite/post/flyer_create.py
+++ OnMuse/MySite/post/flyer_create.py
@@ -1,4 +1,4 @@
-from PIL import Image,ImageDraw,ImageFont
+from PIL import Image,ImageDraw,ImageFont,ImageFilter
 import uuid,unicodedata,datetime
 
 def flyer(num,image,title,user):
@@ -199,12 +199,16 @@ def flyer3(image,title,user):
 def flyer4(image,title,user):
     #ユーザ名10文字、タイトル10文字
     image = Image.open(image)#ユーザの画像
-    #origin = Image.open('origin.png')#static画像
-    origin = Image.new('RGBA',(500,705),'white') #で同じことが可能
-    circle = Image.new('RGBA',(500,705),(122,29,100))
+    logo = Image.open('static/picture/logo.png')
+    origin = Image.new('RGBA',(500,705),'black') #で同じことが可能
+    circle = Image.new('L',(480,480),0)
+    new = Image.new("RGBA",(500,705),0)
+    new.paste(logo,(350,610))
     draw = ImageDraw.Draw(circle)
-    draw.ellipse((11,73,489,551),fill = 255)
+    draw.ellipse((90,60,500-110,560-200),fill = 255)
+    circle = circle.filter(ImageFilter.GaussianBlur(50))
 
+    image = image.convert("L")
     image_copy = image.copy()
 
     WIDTH = 480
@@ -222,19 +226,20 @@ def flyer4(image,title,user):
         dif = int((mag * height - HEIGHT) / 2)#はみ出し測定
         image_copy = image_copy.crop((0,dif,WIDTH,HEIGHT + dif))
 
-    origin.paste(image_copy,(10,72))#貼り付け
-    origin = Image.alpha_composite(origin,circle)
+    origin.paste(image_copy,(10,72),circle)
+    new.paste(logo,(350,610))#貼り付け
+    origin = Image.alpha_composite(origin,new)
 
     draw = ImageDraw.Draw(origin)
     #もしタイトルが奇数だったら9埋めにしてずらす
     #偶数の場合今まで通り10埋め
     font = ImageFont.truetype("msgothic.ttc",50)
     if len_count(title) % 2 == 0:
-        draw.text((0,580),title.center(20 - ZEN_count(title),' '),fill='black',font = font)
+        draw.text((0,545),title.center(20 - ZEN_count(title),' '),fill='white',font = font)
     else:
-        draw.text((12,580),title.center(19 - ZEN_count(title),' '),fill='black',font = font)
+        draw.text((12,545),title.center(19 - ZEN_count(title),' '),fill='white',font = font)
     font = ImageFont.truetype("msgothic.ttc",30)
-    draw.text((0,650),user.rjust(32,' '),fill='black',font = font)
+    draw.text((15,650),user.ljust(32,' '),fill='white',font = font)
 
     name = "medias/flyers/{}.png".format(str(uuid.uuid4()))
     origin.save(name,quality = 95)#保存先のパス
