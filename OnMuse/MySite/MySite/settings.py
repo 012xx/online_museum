7f5094656e5a2bfed4f939c16a197f001efd3ac6
Tanakaryuki <46374705+Tanakaryuki@users.noreply.github.com>
2021-12-11 01:35:58 +0900

    デバッグモードを解除

--- OnMuse/MySite/MySite/settings.py
+++ OnMuse/MySite/MySite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pw69x+rco#)k=euang6duicz_cpw&b)pa-8utuejoa&eb4hscr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['59.106.218.64','localhost','127.0.0.1','www.onmuse.ml']
 
